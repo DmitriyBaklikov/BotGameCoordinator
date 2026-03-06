d0d7484d4368b4c09e4447a5a18575e3b2672f6d
Dmitriy <dmitriy@jetruby.com>
2026-03-06 18:45:17 +0300

    Add background jobs for game threshold checks and auto-archiving expired games

---
+++ config/initializers/telegram.rb
@@ -0,0 +1,6 @@
+Telegram.bots_config = {
+  default: {
+    token: Rails.application.secrets.telegram_bot_token
+  }
+}
+
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  telegram_webhook TelegramWebhooksController
+end
+
